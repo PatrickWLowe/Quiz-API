938adbf5ce8a43dc93dd6e6567c1231cd307de43
Pat Lowe <lowe-patrick@att.net>
2023-04-17 18:05:12 -0700

    Added html and some javascript variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,24 +11,31 @@ var questions = [
   },
   {
     title: 'Arrays in JavaScript can be used to store ____.',
-    choices: [
-      'numbers and strings',
-      'other arrays',
-      'booleans',
-      'all of the above',
-    ],
+    choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above',],
     answer: 'all of the above',
   },
   {
-    title:
-      'String values must be enclosed within ____ when being assigned to variables.',
+    title: 'String values must be enclosed within ____ when being assigned to variables.',
     choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
     answer: 'quotes',
   },
   {
-    title:
-      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
     choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
     answer: 'console.log',
   },
 ];
+
+var start = document.querySelector(".start-div");
+var startquiz = document.querySelector("#press-start");
+var timer = document.querySelector(".timer");
+var QOAbox = document.querySelector("#QOA");
+var qholder = document.querySelector("#Q-holder");
+var OptA = document.querySelector("#OptA");
+var OptB = document.querySelector("#OptB");
+var OptC = document.querySelector("#OptC");
+var OptD = document.querySelector("#OptD");
+var highscorebtn = document.querySelector("#highscore-button");
+var OptD = document.querySelector("#Possibleanswers");
+
+var timeremaining = questions.length * 15;
